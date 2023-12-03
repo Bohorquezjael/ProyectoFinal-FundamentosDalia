28d31bd3ebd3911536a65f52936ff4208b879501
Bohorquez <jaelbkz@icloud.com>
2023-12-03 02:45:12 -0600

    v0.2.0

--- Main.java
+++ Main.java
@@ -49,18 +49,19 @@ public class Main {
 
     public static int Opcion() {
         System.out.println("MENÚ PRINCIPAL");
-        System.out.println("-------------------------------------------------------------------");
-        System.out.println("0.Salir");
-        System.out.println("1.Encontrar a una persona por su nombre");
-        System.out.println("2.Encontrar las personas con su año de nacimiento");
-        System.out.println("3.Encontar las personas con mayor edad por su año de nacimiento");
-        System.out.println("4.Encontar las personas con menor edad por su año de nacimiento");
-        System.out.println("5.Encontrar personas que sus nombres empicen con la letra indicada");
-        System.out.println("6.Encontrar a personas por su estatura");
-        System.out.println("7.Encontrar a personas que sean más bajas que una estatura dada");
-        System.out.println("8.Encontrar a personas que sean más altas que una estatura dada");
-        System.out.println("9.Encontrar la persona más joven");
-        System.out.println("10.Agregar a una persona");
+        System.out.println("+-----------------------------------------------------------------------------------+");
+        System.out.println("|\t\t 0.Salir                                                            |");
+        System.out.println("|\t\t 1.Encontrar a una persona por su nombre                            |");
+        System.out.println("|\t\t 2.Encontrar las personas con su año de nacimiento                  |");
+        System.out.println("|\t\t 3.Encontar las personas con mayor edad por su año de nacimiento    |");
+        System.out.println("|\t\t 4.Encontar las personas con menor edad por su año de nacimiento    |");
+        System.out.println("|\t\t 5.Encontrar personas que sus nombres empicen con la letra indicada |");
+        System.out.println("|\t\t 6.Encontrar a personas por su estatura                             |");
+        System.out.println("|\t\t 7.Encontrar a personas que sean más bajas que una estatura dada    |");
+        System.out.println("|\t\t 8.Encontrar a personas que sean más altas que una estatura dada    |");
+        System.out.println("|\t\t 9.Encontrar la persona más joven                                   |");
+        System.out.println("|\t\t 10.Agregar a una persona                                           |");
+        System.out.println("+-----------------------------------------------------------------------------------+");
         int opc = read.nextInt();
         return opc;
     }
@@ -71,9 +72,9 @@ public class Main {
         String arre[] = new String[3];
         String nomIngresado = read.nextLine();
         boolean encontrado = false;
-        for (int i = 0; i < 10 && encontrado == false; i++) {
+        for (int i = 0; i < 10 && encontrado == false; i++) {//no es necesario el boolean ni nada similar solo es buena practica y eficiente, ademas se pudiese quedar tal como lo tenias y manejar esa exception con un bloque try-catch, en donde con el catch atrapas la exception e imprimes que no fue encontrado(te ahorras condiciones y algo de codigo) pero no creo que a dalia le parezca buena idea en fin esa es otra opción 
             for (int y = 0; y < 6; y++) {
-                if (nomIngresado.equals(Nombres[i][y])) {
+                if (nomIngresado.equals(Nombres[i][y])) {//la excepción que daba era pq cuando no se encuentra el nombre, la matriz se recorre por completo, sin embargo como la matriz también contiene elementos null´s cuando llegas a uno de esos elementos y tratas de llamar al método .equals(Objeto), aquí es cuando te da un error ya que no puedes llamar al método con un Objeto que sea null, resumiendo solo invertí la condición  
                     encontrado = true;
                     arre[0] = Nombres[i][y];
                     arre[1] = Integer.toString(Nacimiento[i][y]);
@@ -81,13 +82,16 @@ public class Main {
                 }
             }
         }
-        if (Arrays.stream(arre).allMatch(Objects::nonNull)) {
+        if (Arrays.stream(arre).allMatch(Objects::nonNull)) {//para que dalia no diga nada se puede recorrer el arreglo y si las posiciones son vacías o null, pd no es necesario recorrer todo el arreglo ya que en la lógica siempre se llena el arreglo o no por lo que podríamos solo hacer una condición que verifique si la primer posición es null  
             for (int x = 0; x < arre.length; x++) {
                 System.out.println(arre[x]);
             }
         } else {
             System.out.println(
                     "El nombre que intentas buscar no se encuentra en mi base de datos, deseas agregarlo ahora? (y/N)");
+                    //llamas a tu método
+                    //agregar elemento 
+                    //primero se tendría que verificar una posición vacía etc...
         }
     }
 
